65e7cabaf6254621738c379d98d8a04992cf06b5
marcellasnegro <78921547+marcellasnegro@users.noreply.github.com>
2021-04-01 19:13:53 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -1,19 +1,23 @@
 //variaveis da led
 const int vermelho = 5;
 const int verde = 6;
-const int azul = 7
+const int azul = 7;
 
 bool estadoLedVermelho = false;
 
+// adiçao de um botao
 const int botao1 = 2;
+const int botao2= 3;
 unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+
 const int botaoDelay = 100;
 
 void setup()
 {
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  
+//saida led azul
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
   pinMode(azul, saida);
